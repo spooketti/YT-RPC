04b8a6fc566361e72bc3e013ae2fde9f9f2015e2
spooketti <jonliu168@gmail.com>
2025-03-02 10:52:46 -0800

    requirements

--- static/src/watch.js
+++ static/src/watch.js
@@ -1,10 +1,3 @@
-let javaURI;
-if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
-    javaURI = "ws://localhost:8085/websocket";
-} else {
-    javaURI = "wss://spring2025.nighthawkcodingsociety.com/websocket";
-}
-
 const servers = {
     iceServers: [
         {
@@ -25,23 +18,19 @@ const servers = {
     iceCandidatePoolSize: 10,
 };
 
-const socket = new WebSocket(javaURI);
+const socket = new WebSocket("ws://localhost:8765");
 let videoStreamGlobal;
 let globalPeer;
 
 socket.onmessage = async function (event) {
     const messageData = JSON.parse(event.data);
     switch (messageData["context"]) {
-        case "viewerOfferServer":
-            viewerOfferServer(messageData);
-            break;
         case "viewerAcceptServer":
             viewerAcceptServer(messageData);
             break;
-        case "iceToStreamerServer":
         case "iceToViewerServer":
             globalPeer.addIceCandidate(new RTCIceCandidate(JSON.parse(messageData["candidate"])));
-            break;
+        break;
     }
 };
 
@@ -51,61 +40,6 @@ function sendMessage(message) {
     } else {
         console.error("WebSocket connection is not open.");
     }
-}
-
-async function viewerOfferServer(messageData) {
-    const peer = new RTCPeerConnection(servers);
-    globalPeer = peer;
-    
-    let remotedesc = new RTCSessionDescription({
-        type: "offer",
-        sdp: messageData["sdp"]
-    });
-    
-    peer.onicecandidate = (e) => {
-        if (e.candidate) {
-            sendMessage({ context: "iceToViewerClient", candidate: JSON.stringify(e.candidate.toJSON()) });
-        }
-    };
-    
-    await peer.setRemoteDescription(remotedesc);
-    videoStreamGlobal.getTracks().forEach(track => peer.addTrack(track, videoStreamGlobal));
-    const answer = await peer.createAnswer();
-    await peer.setLocalDescription(answer);
-    
-    sendMessage({
-        context: "viewerAcceptClient",
-        sdp: answer.sdp,
-        returnID: messageData["returnID"]
-    });
-}
-
-async function captureScreen() {
-    try {
-        let mediaStream = await navigator.mediaDevices.getDisplayMedia({
-            video: { cursor: "always" },
-            audio: false
-        });
-        
-        document.getElementById("streamOffline").style.display = "none";
-        document.getElementById("mortStream").style.display = "block";
-        document.getElementById("mortStream").srcObject = mediaStream;
-        
-        document.getElementById("endBroadcastButton").style.display = "flex";
-        document.getElementById("broadcastButton").style.display = "none";
-        
-        return mediaStream;
-    } catch (ex) {
-        console.log("Error occurred", ex);
-        document.getElementById("endBroadcastButton").style.display = "none";
-    }
-}
-
-async function broadcast() {
-    const stream = await captureScreen();
-    videoStreamGlobal = stream;
-    document.getElementById("mortStream").srcObject = stream;
-    sendMessage({ context: "broadcastRequest" });
 }
 
 function viewerAcceptServer(messageData) {
@@ -128,15 +62,13 @@ function viewerAcceptServer(messageData) {
         });
     
     globalPeer.ontrack = (event) => {
-        document.getElementById("mortStream").srcObject = event.streams[0];
-        document.getElementById("mortStream").style.display = "block";
-        document.getElementById("streamOffline").style.display = "none";
+        document.getElementById("audioElement").srcObject = event.streams[0];
     };
 }
 
 async function watch() {
     const peer = new RTCPeerConnection(servers);
-    peer.addTransceiver("video", { direction: "recvonly" });
+    peer.addTransceiver("audio", { direction: "recvonly" });
     const offer = await peer.createOffer();
     await peer.setLocalDescription(offer);
     
